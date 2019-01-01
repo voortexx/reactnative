8be6186e7b5496b0fa57fb10f8beca3ea2a23207
Fabien Raymond <voortexxx@gmail.com>
2019-01-01 15:12:33 +0100

    add personal icons on homepage

--- Components/Home.js
+++ Components/Home.js
@@ -1,12 +1,66 @@
 import React from "react";
 import Logo from "./Logo";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Linking, Text, FlatList } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 class Home extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Logo />
+        <View
+          style={{ flex: 6, alignItems: "center", justifyContent: "center" }}
+        >
+          <Logo />
+        </View>
+
+        <View
+          style={{ flex: 2, alignItems: "center", justifyContent: "center" }}
+        >
+          <Text style={{ marginTop: 20, color: "#201F76" }}>
+            Made by <Text style={{ fontWeight: "bold" }}>Fabien Raymond</Text>
+          </Text>
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            <Text
+              style={{ margin: 5 }}
+              onPress={() =>
+                Linking.openURL(
+                  "http://https://www.facebook.com/fabien.raymond.98.fr"
+                ).catch(err => console.error("An error occurred", err))
+              }
+            >
+              <Ionicons
+                name="logo-facebook"
+                style={{ fontSize: 30, color: "#201F76" }}
+              />
+            </Text>
+            <Text
+              style={{ margin: 5 }}
+              onPress={() =>
+                Linking.openURL(
+                  "https://www.linkedin.com/in/fabien-raymond-41227114b/"
+                ).catch(err => console.error("An error occurred", err))
+              }
+            >
+              <Ionicons
+                name="logo-linkedin"
+                style={{ fontSize: 30, color: "#201F76" }}
+              />
+            </Text>
+            <Text
+              style={{ margin: 5 }}
+              onPress={() =>
+                Linking.openURL("http://fabienraymond.fr").catch(err =>
+                  console.error("An error occurred", err)
+                )
+              }
+            >
+              <Ionicons
+                name="md-globe"
+                style={{ fontSize: 30, color: "#201F76" }}
+              />
+            </Text>
+          </View>
+        </View>
       </View>
     );
   }
