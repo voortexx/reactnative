d55fb438d5ca94cc1b750134432eb58f8afb9ab5
Fabien Raymond <voortexxx@gmail.com>
2019-01-03 15:07:02 +0100

    add style to filter

--- Components/Effectif.js
+++ Components/Effectif.js
@@ -5,7 +5,8 @@ import {
   TextInput,
   FlatList,
   ActivityIndicator,
-  Button
+  TouchableOpacity,
+  Text
 } from "react-native";
 import axios from "axios";
 import Player from "./Player";
@@ -79,31 +80,82 @@ class Effectif extends Component {
           onChangeText={text => this.searchPlayer(text)}
           onSubmitEditing={() => this.getPlayersBySearch()}
         />
-        <Button
-          title="Forward"
-          color="#841584"
-          onPress={() => this.changeFilter("Forward")}
-        />
-        <Button
-          title="Defense"
-          color="#841584"
-          onPress={() => this.changeFilter("Defense")}
-        />
-        <Button
-          title="Goalkeeper"
-          color="#841584"
-          onPress={() => this.changeFilter("Goalkeeper")}
-        />
-        <Button
-          title="Midfielder"
-          color="#841584"
-          onPress={() => this.changeFilter("Midfielder")}
-        />
-        <Button
-          title="All"
-          color="#841584"
-          onPress={() => this.changeFilter(null)}
-        />
+        <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+          <TouchableOpacity
+            title="All"
+            color="#841584"
+            style={{ margin: 5 }}
+            onPress={() => this.changeFilter(null)}
+          >
+            <Text
+              style={[
+                this.state.filterValue === null && styles.bold,
+                { fontSize: 12 }
+              ]}
+            >
+              All
+            </Text>
+          </TouchableOpacity>
+          <View style={{ flexDirection: "row" }}>
+            <TouchableOpacity
+              color="#841584"
+              style={{ margin: 5 }}
+              onPress={() => this.changeFilter("Goalkeeper")}
+            >
+              <Text
+                style={[
+                  this.state.filterValue === "Goalkeeper" && styles.bold,
+                  { fontSize: 12 }
+                ]}
+              >
+                Goalkeeper
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              color="#841584"
+              style={{ margin: 5 }}
+              onPress={() => this.changeFilter("Defender")}
+            >
+              <Text
+                style={[
+                  this.state.filterValue === "Defender" && styles.bold,
+                  { fontSize: 12 }
+                ]}
+              >
+                Defender
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              color="#841584"
+              style={{ margin: 5 }}
+              onPress={() => this.changeFilter("Midfielder")}
+            >
+              <Text
+                style={[
+                  this.state.filterValue === "Midfielder" && styles.bold,
+                  { fontSize: 12 }
+                ]}
+              >
+                Midfielder
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              color="#841584"
+              style={{ margin: 5 }}
+              onPress={() => this.changeFilter("Forward")}
+            >
+              <Text
+                style={[
+                  this.state.filterValue === "Forward" && styles.bold,
+                  { fontSize: 12 }
+                ]}
+              >
+                Forward
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
         <FlatList
           data={
             this.state.filter.length > 0
@@ -126,9 +178,7 @@ class Effectif extends Component {
 }
 
 const styles = StyleSheet.create({
-  main_container: {
-    flex: 1
-  },
+  main_container: {},
   textinput: {
     marginLeft: 5,
     marginRight: 5,
@@ -146,6 +196,9 @@ const styles = StyleSheet.create({
     bottom: 0,
     alignItems: "center",
     justifyContent: "center"
+  },
+  bold: {
+    fontWeight: "bold"
   }
 });
 
